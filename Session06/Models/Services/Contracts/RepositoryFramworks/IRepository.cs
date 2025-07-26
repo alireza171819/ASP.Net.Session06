a78482b14d6619898cb684b7d578651d9bdc7be3
alireza171819 <alirezanabegheh6565@gmail.com>
2025-07-26 22:00:52 +0430

    update IRepository

--- Session06/Models/Services/Contracts/RepositoryFramworks/IRepository.cs
+++ Session06/Models/Services/Contracts/RepositoryFramworks/IRepository.cs
@@ -5,11 +5,16 @@ namespace Session06.Models.Services.Contracts.RepositoryFramworks
     public interface IRepository<T> where T : class
     {
         /// <summary>
-        /// Gets all products.
+        /// Insert a new product to the repository.
         /// </summary>
-        /// <returns>A collection of products.</returns>
-        void Add(T objModel);
-        T? GetId(int id);
+        /// <param name="product">The product to add.</param>
+        void Insert(T objModel);
+        /// <summary>
+        /// Gets a product by its identifier.
+        /// </summary>
+        /// <param name="id">The identifier of the product.</param>
+        /// <returns>The product with the specified identifier, or null if not found.</returns>
+        T? SelectById(int id);
         T? Select(Expression<Func<T, bool>> predicate);
         IEnumerable<T> SelectList(Expression<Func<T, bool>> where = null);
         IEnumerable<T> SelectList(Expression<Func<T, bool>> where = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null, string includes = "");
